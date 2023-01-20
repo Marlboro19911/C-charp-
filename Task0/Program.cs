a6d341fb8a66e401f6b0ae1a64983c57bfae549c
Yakov <Marlboro19911@gmail.com>
2023-01-20 18:44:45 +0300

    добавил Task0

---
+++ Task0/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число 
+// и выдаёт его квадрат (число умноженное на само себя).
+
+Console.Write("Введите число > ");
+string strValue = Console.ReadLine();
+int value;
+value = Convert.ToInt32(strValue); 
+
+int result = value * value;
+
+System.Console.WriteLine($"Квадрат числа {value} равен {result} "); 
